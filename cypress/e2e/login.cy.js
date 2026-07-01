b24939617a8ec7323a122a571b842ff412e67370
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-07-01 20:25:30 -0300

    cy.session

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@ describe('Login', () => {
     it('successfully logs in', () => {
         cy.intercept('GET', '**/notes').as('getNotes')
 
-        cy.login()
+        cy.guiLogin()
         cy.wait('@getNotes')
 
         cy.contains('a', 'Create a new note').should('be.visible')

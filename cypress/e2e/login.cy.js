1f70f39ccbb416810a64968be4d6acc199f7d023
vandersonwin111tb <vandersonwin111tb@gmail.com>
2026-07-01 22:39:14 -0300

    fix: corrige indentacao do arquivo login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,10 @@
 describe('Login', () => {
-    it('successfully logs in', () => {
-        cy.intercept('GET', '**/notes').as('getNotes')
+  it('successfully logs in', () => {
+    cy.intercept('GET', '**/notes').as('getNotes')
 
-        cy.guiLogin()
-        cy.wait('@getNotes')
+    cy.guiLogin()
+    cy.wait('@getNotes')
 
-        cy.contains('a', 'Create a new note').should('be.visible')
-    })
+    cy.contains('a', 'Create a new note').should('be.visible')
+  })
 })
